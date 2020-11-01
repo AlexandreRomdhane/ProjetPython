616ecf60d4bcb6567fd1acaa79f04b6d594a958c
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2020-11-01 13:29:53 +0100

    Design page de connexion et création de compte
    Création de deux dossiers, le premier dossier pour stocker les templates et le deuxième pour stocker les fichiers statiques tels que le css et le js.
    
    Vérification de la force du mot de passe et la validation de l'adresse mail.
    
    Ajout de la date de création du compte dans la base de données.
    
    Design des pages de connexion et de création de compte.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from flask import Flask, session, redirect, url_for, request
+from flask import Flask, session, redirect, url_for, request, render_template
 from markupsafe import escape
 import pymysql.cursors
 import bcrypt
+from email_validator import validate_email, EmailNotValidError
+import re
+from datetime import date
 
 app = Flask(__name__)
 
 app.secret_key = "OuiBiensurquecestsecretsinonsasersarien"
 
 requestSQLIfUserExist = "SELECT NomUtilisateur FROM utilisateur WHERE Mail = %s"
-requestSQLInsertUser = "INSERT INTO utilisateur (Mail, MotDePasse, NomUtilisateur) VALUES (%s, %s, %s)"
+requestSQLInsertUser = "INSERT INTO utilisateur (Mail, MotDePasse, NomUtilisateur, DateCreation) VALUES (%s, %s, %s, %s)"
 requestSQLLoginUser = "SELECT UtilisateurID, MotDePasse, NomUtilisateur FROM utilisateur WHERE Mail = %s"
+
+charLengthRegex = re.compile(r'(\w{8,})')
+upperRegex = re.compile(r'[A-Z]+')
 
 
 # Fonction qui renvoie la connexion a la base (interface)
@@ -29,51 +35,71 @@ connectionDB = get_connection()  # Objet de la connexion MySQL
 @app.route("/")
 def index():
     if 'id' in session:
-        return 'Logged in as %s' % escape(session['id'])
+        return 'Tu es connecté en tant que %s' % escape(session['id'])
     return redirect(url_for('login'))
 
 
 # Route pour s'enregistrer, GET pour avoir le formulaire et POST pour soumettre les données
-# TODO Faire une vérif sur la force du mot de passe avant de l'enregistrer
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        # Formulaire d'enregistrement (a faire dans un template)
-        return '''
-                <form method="post">
-                    <p><input type=text name=email>
-                    <p><input type=password name=password>
-                    <p><input type=text name=user>
-                    <p><input type=submit value=Register>
-                </form>
-            '''
+        if not session.get('id') is None:
+            return redirect(url_for('index'))
+        else:
+            return render_template('register.html')
     else:
+        error = []  # Liste des erreurs a passer a la template
         try:
-            with connectionDB.cursor() as cur:
-                # On regarde en base si le compte n'existe pas en fonction de l'adresse mail
-                cur.execute(requestSQLIfUserExist, request.form['email'])
-                if cur.rowcount == 0:  # Si aucun enregistrement ne correspond alors on crée le compte
-                    with connectionDB.cursor() as cursorInsert:
-                        # On enregistre l'utilisateur en base en hashant sont mot de passe
-                        cursorInsert.execute(requestSQLInsertUser,
-                                             (request.form['email'],
-                                              bcrypt.hashpw(request.form['password'].encode('utf8'), bcrypt.gensalt()),
-                                              request.form['user']))
-                        connectionDB.commit()  # Permet de valider la transaction
-                        return redirect(url_for('login'))  # Une fois finis on le redirige vers la page de connexion
-                else:
-                    # Si l'adresse mail est déjà utilisée
-                    return 'Le compte existe déjà'
+            # Valide l'adresse email si elle n'est pas valide une exception seras lancée (EmailNotValidError)
+            error = check_password(request.form['password'])
+            if len(request.form['user']) == 0:
+                error.append("Le nom d'utilisateur ne doit pas être vide !")
+            validate_email(request.form['email'])
+            if len(error) == 0:  # Si il y a aucune erreur alors on continue
+                with connectionDB.cursor() as cur:
+                    # On regarde en base si le compte n'existe pas en fonction de l'adresse mail
+                    cur.execute(requestSQLIfUserExist, request.form['email'])
+                    if cur.rowcount == 0:  # Si aucun enregistrement ne correspond alors on crée le compte
+                        with connectionDB.cursor() as cursorInsert:
+                            # On enregistre l'utilisateur en base en hashant sont mot de passe
+                            cursorInsert.execute(requestSQLInsertUser,
+                                                 (request.form['email'],
+                                                  bcrypt.hashpw(request.form['password'].encode('utf8'),
+                                                  bcrypt.gensalt()),
+                                                  request.form['user'],
+                                                  date.today()))
+                            connectionDB.commit()  # Permet de valider la transaction
+                            return redirect(url_for('login'))  # Une fois finis on le redirige vers la page de connexion
+                    else:
+                        # Si l'adresse mail est déjà utilisée
+                        # TODO Faire une fonction pour éviter de faire plusieurs fois le même traitement
+                        error.append('Le compte existe déjà')
+                        return render_template('register.html'
+                                               , error=error
+                                               , email=request.form['email'], user=request.form['user'])
+            else:
+                # TODO Faire une fonction pour éviter de faire plusieurs fois le même traitement
+                return render_template('register.html'
+                                       , error=error
+                                       , email=request.form['email'], user=request.form['user'])
         except pymysql.Error as e:
             # En cas d'erreur voir sur internet pour mieux les gérer
             print(e)
-            return 'Erreur'
+            error.append('Une erreur SQL est arrivée')
+            return render_template('register.html', error=error)
+        except EmailNotValidError as e:
+            error.append("L'adresse email n'est pas valide !")
+            # TODO Faire une fonction pour éviter de faire plusieurs fois le même traitement
+            return render_template('register.html'
+                                   , error=error
+                                   , email=request.form['email'], user=request.form['user'])
 
 
 # Route pour se connecter, GET pour avoir le formulaire et POST pour soumettre les données
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        error = []
         # Faire la vérification
         try:
             with connectionDB.cursor() as cursorLogin:
@@ -85,27 +111,23 @@ def login():
                     if bcrypt.checkpw(request.form['password'].encode('utf8'), result['MotDePasse'].encode('utf8')):
                         # Le mot de passe est correcte
                         session['id'] = result['UtilisateurID']
-                        return 'Tu es connecté !'
+                        return redirect(url_for('index'))  # Page du fil d'actualité
                     else:
-                        return "Le mot de passe n'est pas correcte !"
+                        error.append("Le mot de passe ou l'adresse email n'est pas valide !")
+                        return render_template('login.html', error=error, email=request.form['email'])
                 else:
-                    return "Le compte n'existe pas !"
+                    error.append("Le mot de passe ou l'adresse n'est pas valide !")
+                    return render_template('login.html', error=error, email=request.form['email'])
         except pymysql.Error as e:
             # En cas d'erreur voir sur internet pour mieux les gérer
             print(e)
-            return "Erreur"
+            error.append("Erreur SQL")
+            return render_template('login.html', error=error, email=request.form['email'])
     else:
         if not session.get('id') is None:
             return redirect(url_for('index'))
         else:
-            # Formulaire de connexion (a faire dans un template)
-            return '''
-                    <form method="post">
-                        <p><input type=text name=email>
-                        <p><input type=password name=password>
-                        <p><input type=submit value=Login>
-                    </form>
-                '''
+            return render_template('login.html')
 
 
 # Déconnexion du compte
@@ -113,3 +135,12 @@ def login():
 def logout():
     session.pop('id', None)
     return redirect(url_for('login'))
+
+
+def check_password(password):
+    val_ret = []
+    if not charLengthRegex.findall(password):
+        val_ret.append("Le mot de passe doit contenir au moins 8 caractères !")
+    if not upperRegex.findall(password):
+        val_ret.append("Le mot de passe doit contenir au moins une majuscule !")
+    return val_ret
