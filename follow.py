bd2082fb754a172d802e53082ab03140e915a050
AlexandreRomdhane <alexandre.romdhane@ipilyon.net>
2020-11-15 19:04:20 +0100

    Correction de la fonction follow
    Fonction follow
    Création de la requête select utilisateurid

--- follow.py
+++ follow.py
@@ -9,43 +9,53 @@ requestSQLDeleteFollower = """Delete FROM Followers WHERE Follower_id = %s
 
 requestSQLIfFollowerExist = """SELECT Followed_id FROM Followers WHERE Follower = %s"""
 
+requestSQLUsername = """SELECT UtilisateurID FROM Utilisateur WHERE NomUtilisateur = %s"""
+
 
 def follow(username):
-    if request.method == 'GET':
-        if not session.get('id') is None:
-            return redirect(url_for('index'))
-        else:
-            return render_template('register.html')
+    if not session.get('id') is None:
+        return redirect(url_for('index'))
     else:
-        error = []  # Liste des erreurs a passer dans le template
-        try:
-            if len(request.form['followed_id']) == 0:
-                error.append("Aucune personne à suivre")
-                return render_template('profil.html')
-            if len(error) == 0: 
-                # Si il y a aucune erreur alors on continue
-                with connectionDB.cursor() as cur:
-                    # Vérification dans la BDD si le user ne suit pas déjà la personne
-                    cur.execute(requestSQLIfFollowerExist, session.get('id'))
-                    if cur.rowcount == 0: 
-                        # Cas où le user ne suit pas
-                        with connectionDB.cursor() as cursorInsert:
-                            # Enregistrement du follow
-                            cursorInsert.execute(requestSQLInsertFollower, 
-                            session.get('id'),
-                            request.form['followed_id'])
-                            connnectionDB.commit() # Permet de valider la transation
-                            return render_template('profil.html')
-                    else:
-                        # Cas où le user suit déjà la personne
-                        #
-                        error.append('Vous suivez déjà la personne')
-                        return render_template('profil.html')
-            else:
-                #
-                return render_template('profil.html')
-        except pymysql.Error as e:
+        return render_template('register.html')
+    error = []  # Liste des erreurs a passer dans le template
+    try:
+        if len(username) == 0:
+            error.append("Aucune personne à suivre")
+            print(error)
             return render_template('profil.html')
+        if len(error) == 0: 
+            # Si il y a aucune erreur alors on continue
+            with connectionDB.cursor() as cur:
+                # Vérification dans la BDD si le user ne suit pas déjà la personne
+                cur.execute(requestSQLIfFollowerExist, session.get('id'))
+                if cur.rowcount == 0: 
+                    # Cas où le user ne suit pas
+                    with connectionDB.cursor() as cursorSelect:
+                        # Selection du followed_id
+                        cursorInsert.execute(requestSQLUsername, 
+                        username)
+                        followed_id = cursorSelect.fetchall()
+                    with connectionDB.cursor() as cursorInsert:
+                        # Enregistrement du follow
+                        cursorInsert.execute(requestSQLInsertFollower, 
+                        session.get('id'),
+                        followed_id)
+                        connectionDB.commit() # Permet de valider la transation
+                        print(error)
+                        return render_template('profil.html')
+                else:
+                    # Cas où le user suit déjà la personne
+                    #
+                    error.append('Vous suivez déjà la personne')
+                    print(error)
+                    return render_template('profil.html')
+        else:
+            #
+            print(error)
+            return render_template('profil.html')
+    except pymysql.Error as e:
+        print(error)
+        return render_template('profil.html')
 
 def unfollow(username):
     if request.method == 'GET':
@@ -56,7 +66,7 @@ def unfollow(username):
     else:
         test = []  # Liste des erreurs a passer dans le template
         try:
-            if len(request.form['followed_id']) != 0:
+            if len(username) != 0:
                 test.append("Vous ne suivez pas cette personne")
             if len(test) != 0: 
                 # Si il y a aucune erreur alors on continue
@@ -65,11 +75,16 @@ def unfollow(username):
                     cur.execute(requestSQLIfFollowerExist, session.get('id'))
                     if cur.rowcount == 1: 
                         # Cas où le user suit déjà
+                        with connectionDB.cursor() as cursorSelect:
+                            # Selection du followed_id
+                            cursorInsert.execute(requestSQLUsername, 
+                            username)
+                            followed_id = cursorSelect.fetchall()
                         with connectionDB.cursor() as cursorInsert:
                             # Supression du follow
                             cursorInsert.execute(requestSQLDeleteFollower, 
                             session.get('id'),
-                            request.form['followed_id'])
+                            followed_id)
                             connnectionDB.commit() # Permet de valider la transation
                             return redirect(url_for('profil'))
                     else:
