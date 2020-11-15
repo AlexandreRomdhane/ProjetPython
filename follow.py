90ec26a8a7622f197529943bbe9e20b648f1a0cc
AlexandreRomdhane <alexandre.romdhane@ipilyon.net>
2020-11-15 19:48:59 +0100

    Modification de la fonction unfollow
    Mod fonction unfollow
    Minor mod fonction follow

--- follow.py
+++ follow.py
@@ -7,69 +7,56 @@ requestSQLInsertFollower = """INSERT INTO Followers (Follower_id, Followed_id)
 requestSQLDeleteFollower = """Delete FROM Followers WHERE Follower_id = %s 
                         AND Followed_id = %s"""
 
-requestSQLIfFollowerExist = """SELECT Followed_id FROM Followers WHERE Follower = %s"""
+requestSQLIfFollowerExist = """SELECT Followed_id FROM Followers WHERE Follower_id = %s"""
 
 requestSQLUsername = """SELECT UtilisateurID FROM Utilisateur WHERE NomUtilisateur = %s"""
 
 
 def follow(username):
-    if not session.get('id') is None:
-        return redirect(url_for('index'))
-    else:
+    if session.get('id') < 0:
+        print(session.get('id'))
         return render_template('register.html')
-    error = []  # Liste des erreurs a passer dans le template
-    try:
-        if len(username) == 0:
-            error.append("Aucune personne à suivre")
-            print(error)
-            return render_template('profil.html')
-        if len(error) == 0: 
-            # Si il y a aucune erreur alors on continue
-            with connectionDB.cursor() as cur:
-                # Vérification dans la BDD si le user ne suit pas déjà la personne
-                cur.execute(requestSQLIfFollowerExist, session.get('id'))
-                if cur.rowcount == 0: 
-                    # Cas où le user ne suit pas
-                    with connectionDB.cursor() as cursorSelect:
-                        # Selection du followed_id
-                        cursorInsert.execute(requestSQLUsername, 
-                        username)
-                        followed_id = cursorSelect.fetchall()
-                    with connectionDB.cursor() as cursorInsert:
-                        # Enregistrement du follow
-                        cursorInsert.execute(requestSQLInsertFollower, 
-                        session.get('id'),
-                        followed_id)
-                        connectionDB.commit() # Permet de valider la transation
-                        print(error)
-                        return render_template('profil.html')
-                else:
-                    # Cas où le user suit déjà la personne
-                    #
-                    error.append('Vous suivez déjà la personne')
-                    print(error)
-                    return render_template('profil.html')
-        else:
-            #
-            print(error)
-            return render_template('profil.html')
-    except pymysql.Error as e:
-        print(error)
-        return render_template('profil.html')
-
-def unfollow(username):
-    if request.method == 'GET':
-        if not session.get('id') is None:
-            return redirect(url_for('index'))
-        else:
-            return render_template('register.html')
-    else:
-        test = []  # Liste des erreurs a passer dans le template
+    else:     
         try:
             if len(username) != 0:
-                test.append("Vous ne suivez pas cette personne")
-            if len(test) != 0: 
-                # Si il y a aucune erreur alors on continue
+                with connectionDB.cursor() as cur:
+                    # Vérification dans la BDD si le user ne suit pas déjà la personne
+                    cur.execute(requestSQLIfFollowerExist, session.get('id'))
+                    if cur.rowcount == 0: 
+                        # Cas où le user ne suit pas
+                        with connectionDB.cursor() as cursorSelect:
+                            # Selection du followed_id
+                            cursorInsert.execute(requestSQLUsername, 
+                            username)
+                            followed_id = cursorSelect.fetchall()
+                        with connectionDB.cursor() as cursorInsert:
+                            # Enregistrement du follow
+                            cursorInsert.execute(requestSQLInsertFollower, 
+                            session.get('id'),
+                            followed_id)
+                            connectionDB.commit() # Permet de valider la transation
+                            print(error)
+                            return render_template('profil.html')
+                    else:
+                        # Cas où le user suit déjà la personne
+                        #
+                        error.append('Vous suivez déjà la personne')
+                        print("Vous suivez déjà la personne")
+                        return render_template('profil.html')
+            else:
+                #
+                print("error")
+                return render_template('profil.html')
+        except pymysql.Error as e:
+            print("error")
+            return render_template('profil.html')
+
+def unfollow(username):
+    if session.get('id') < 0:
+        return render_template('register.html')
+    else:
+        try:
+            if len(username) == 0:
                 with connectionDB.cursor() as cur:
                     # Vérification dans la BDD si le user ne suit pas déjà la personne
                     cur.execute(requestSQLIfFollowerExist, session.get('id'))
@@ -90,11 +77,13 @@ def unfollow(username):
                     else:
                         # Cas où le user ne suit pas
                         #
-                        error.append('Vous suivez déjà la personne')
+                        error.append('Vous ne suivez pas la personne')
+                        print('Vous ne suivez pas la personne')
                         return render_template('profil.html')
             else:
                 #
                 return render_template('profil.html')
         except pymysql.Error as e:
-            return "Oui"
+            print(error)
+            return render_template('profil.html')
             
